2a07b7f31d79770a018fd2e19790d9205588dc21
Josue Hernandez <moradel82@gmail.com>
2020-10-13 22:41:54 -0700

    Adding Context manager and using local API server

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import CircleButton from '../CircleButton/CircleButton'
-import { countNotesForFolder } from '../notes-helpers'
-import './NoteListNav.css'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CircleButton from '../CircleButton/CircleButton';
+import NotefulContext from '../NotefulContext';
+import { countNotesForFolder } from '../notes-helpers';
+import './NoteListNav.css';
 
-export default function NoteListNav(props) {
-  return (
+
+export default class NoteListNav extends React.Component {
+  static contextType = NotefulContext;
+
+  render () {
+    // Not sure I fully understand this notation?
+    // Is this folders is created but if it doesn exist
+    // it will assign an empty list?
+    // When will this apply to be empty? when notes is undefined?
+    const { folders: foldersxyz=[], notes=[] } = this.context;
+
+    return (
     <div className='NoteListNav'>
       <ul className='NoteListNav__list'>
-        {props.folders.map(folder =>
+        {foldersxyz.map(folder =>
           <li key={folder.id}>
             <NavLink
               className='NoteListNav__folder-link'
               to={`/folder/${folder.id}`}
             >
               <span className='NoteListNav__num-notes'>
-                {countNotesForFolder(props.notes, folder.id)}
+                {countNotesForFolder(notes, folder.id)}
               </span>
               {folder.name}
             </NavLink>
@@ -36,9 +47,5 @@ export default function NoteListNav(props) {
         </CircleButton>
       </div>
     </div>
-  )
-}
-
-NoteListNav.defaultProps = {
-  folders: []
+  )}
 }
